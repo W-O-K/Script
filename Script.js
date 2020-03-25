81378a376e69f0b41412c2fb61a36a0e3ffffb42
Wisdom <61986681+W-O-K@users.noreply.github.com>
2020-03-25 11:40:37 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,8 @@
+var name = "Wisdom Ojiakor";
+var courses = ["html", "css", "javascript"];
+console.log("My name is " + name);
+console.log("My courses are " + courses);
+var myArray = [];
+for (var i = 1; i < 200; i += 2) {
+  myArray.push(i);}
+console.log(myArray);
